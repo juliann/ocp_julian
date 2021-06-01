79f318f550e5882a40989cae669f0c51935e66e3
Julian <julian.nadarz@gmail.com>
2021-06-01 14:37:24 +0200

    cleanup

--- src/loggingStuff/LogTest.java
+++ src/loggingStuff/LogTest.java
@@ -4,9 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LogTest {
+	static { System.setProperty("logback.configurationFile", "./logback.xml");}
 public static void main(String[] args) {
 	
-	 Logger logger = LoggerFactory.getLogger("com.baeldung.logback");
-	    logger.info("Hello {} World ", 6);
+    Logger logger = LoggerFactory.getLogger(LogTest.class);
+    Logger fileLogger = LoggerFactory.getLogger("com.baeldung.logback.tests");
+    logger.info("Hello {} World ", 6);
+    logger.warn("AAAAAAAAAH");
+    fileLogger.warn("AH");
 }
 }
